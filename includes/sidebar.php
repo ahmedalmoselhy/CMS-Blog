ceb0311d6d9d047df0dff9516d36c453986210df
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-06-26 23:11:12 +0200

    showing categories in the sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -20,33 +20,22 @@
 <!-- Blog Categories Well -->
 <div class="well">
     <h4>Blog Categories</h4>
+    <?php
+        $query = "SELECT * FROM categories";
+        $select_all_cats = mysqli_query($connection, $query);
+        
+    ?>
     <div class="row">
         <div class="col-lg-6">
             <ul class="list-unstyled">
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
+                <?php
+                    while($row = mysqli_fetch_assoc($select_all_cats)){
+                        $cat = $row['cat_title'];
+                        echo "<li><a href='#'>{$cat}</a></li>";
+                    }
+                ?>
             </ul>
         </div>
-        <!-- /.col-lg-6 -->
-        <div class="col-lg-6">
-            <ul class="list-unstyled">
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.col-lg-6 -->
     </div>
     <!-- /.row -->
 </div>

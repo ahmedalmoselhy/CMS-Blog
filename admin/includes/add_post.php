7c1b014cff3a136761dbb6d96096464639da9601
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-06-28 00:44:59 +0200

    adding function to move photos inside blog directory

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -2,7 +2,23 @@
     Add Post
     <!-- <small>Subheading</small> -->
 </h1>
+<?php
+    if(isset($_POST['create_post'])){
+        $post_title = $_POST['title'];
+        $post_author = $_POST['post_author'];
+        $post_category = $_POST['post_category'];
 
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']['tmp_name'];
+
+        $post_tags = $_POST['post_tags'];
+        $post_content = $_POST['post_content'];
+        $post_date = date('d-m-y');
+        $post_comment_count = 0;
+
+        move_uploaded_file($post_image_temp, "../images/$post_image");
+    }
+?>
 <div class="container">
 <form class="col-xs-8" action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
@@ -27,7 +43,7 @@
 
     <div class="form-group">
         <label for="post_image">Post Image</label>
-        <input type="file" class="form-control" name="post_image">
+        <input type="file" class="form-control" name="image">
     </div>
 
     <div class="form-group">

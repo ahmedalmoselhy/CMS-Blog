43da3f66886806e0941ab685e09b64ea0dc7c974
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-06-27 22:47:24 +0200

    add a new page to display all posts in admin panel

---
+++ admin/posts.php
@@ -0,0 +1,31 @@
+<?php include "includes/header.php"; ?>
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/navigation_bar.php"; ?>
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            All Posts
+                            <!-- <small>Subheading</small> -->
+                        </h1>
+                        
+                    </div>
+                    
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+<?php include "includes/footer.php"; ?>

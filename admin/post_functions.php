6d81c64a433d1b8e0e9840bbd19e4bfbbee912fb
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-06-28 19:20:11 +0200

    moving the code to view all posts into a function

--- admin/post_functions.php
+++ admin/post_functions.php
@@ -1,4 +1,39 @@
 <?php
+    function showAllPosts(){
+        global $connection;
+        $query = "SELECT * FROM posts";
+        $all_posts = mysqli_query($connection, $query);
+        if (!$all_posts) {
+            die("QUERY FAILED! " . mysqli_error($connection));
+        }
+        while ($row = mysqli_fetch_assoc($all_posts)) {
+            $id = $row['post_id'];
+            $category = $row['post_cat_id'];
+            $title = $row['post_title'];
+            $author = $row['author_id'];
+            $date = $row['post_date'];
+            $image = $row['post_image'];
+            $content = $row['content'];
+            $tags = $row['post_tags'];
+            $comments_count = $row['comments_count'];
+            $post_status = $row['post_status'];
+            // Display Data in Rows
+            echo "<tr>";
+            echo "<td>{$id}</td>";
+            echo "<td>{$category}</td>";
+            echo "<td>{$title}</td>";
+            echo "<td>{$author}</td>";
+            echo "<td>{$date}</td>";
+            echo "<td><img width='100' src='../images/{$image}' alt=''></td>";
+            echo "<td>{$tags}</td>";
+            echo "<td>{$comments_count}</td>";
+            echo "<td>{$post_status}</td>";
+            echo "<td><a href='posts.php?delete={$id}'>Delete Post</a></td>";
+            echo "<td><a href='posts.php?source=edit_post'>Update Post</a></td>";
+            echo "</tr>";
+        }
+    }
+
     function createPost(){
         global $connection;
         $post_title = $_POST['title'];

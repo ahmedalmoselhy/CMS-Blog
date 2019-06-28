22436ff6bf6f53b418f0ae5680bcede2430faf8e
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-06-28 22:53:43 +0200

    making a post page

--- index.php
+++ index.php
@@ -1,57 +1,57 @@
-
 <!-- Header -->
 <?php include "includes/header.php"; ?>
 
-    <!-- Navigation -->
-    <?php include "includes/navigation_bar.php"; ?>
+<!-- Navigation -->
+<?php include "includes/navigation_bar.php"; ?>
 
-    <!-- Page Content -->
-    <div class="container">
+<!-- Page Content -->
+<div class="container">
 
-        <div class="row">
+    <div class="row">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
-                <!-- Generate posts from the database -->
-                <?php
-                    $query = "SELECT * FROM posts";
-                    $all_posts = mysqli_query($connection, $query);
-                        while($row = mysqli_fetch_assoc($all_posts)){
-                            $post_title = $row['post_title'];
-                            $post_content = $row['content'];
-                            $post_author = $row['author_id'];
-                            $post_date = $row['post_date'];
-                            $post_image = $row['post_image'];
-                            ?>
-                            <h2><a href='#'><?php echo $post_title; ?></a></h2>
-                            <p class="lead">
-                            by <a href="index.php"><?php echo $post_author; ?></a>
-                            </p>
-                            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
-                            <hr>
-                            <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                            <hr>
-                            <p><?php echo $post_content; ?></p>
-                            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                    <?php } ?>
-                
-
+            <h1 class="page-header">
+                Page Heading
+                <small>Secondary Text</small>
+            </h1>
+            <!-- Generate posts from the database -->
+            <?php
+            $query = "SELECT * FROM posts";
+            $all_posts = mysqli_query($connection, $query);
+            while ($row = mysqli_fetch_assoc($all_posts)) {
+                $post_id = $row['post_id'];
+                $post_title = $row['post_title'];
+                $post_content = $row['content'];
+                $post_author = $row['author_id'];
+                $post_date = $row['post_date'];
+                $post_image = $row['post_image'];
+                ?>
+                <h2><a href='post.php?p_id=<?php echo $post_id; ?>'><?php echo $post_title; ?></a></h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author; ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
                 <hr>
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                <hr>
+                <p><?php echo $post_content; ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            <?php } ?>
 
 
-            </div>
+            <hr>
 
-            <!-- Blog Sidebar Widgets Column -->
-            <?php include "includes/sidebar.php"; ?>
 
         </div>
-        <!-- /.row -->
 
-        <hr>
-<!-- Footer -->
-<?php include "includes/footer.php"; ?>
+        <!-- Blog Sidebar Widgets Column -->
+        <?php include "includes/sidebar.php"; ?>
+
+    </div>
+    <!-- /.row -->
+
+    <hr>
+    <!-- Footer -->
+    <?php include "includes/footer.php"; ?>
--- post.php
+++ post.php
@@ -1,112 +1,55 @@
 <?php include "includes/header.php"; ?>
 
-    <!-- Navigation -->
-    <?php include "includes/navigation_bar.php"; ?>
+<!-- Navigation -->
+<?php include "includes/navigation_bar.php"; ?>
 
-    <!-- Page Content -->
-    <div class="container">
+<!-- Page Content -->
+<div class="container">
 
-        <div class="row">
+    <div class="row">
 
-            <!-- Blog Post Content Column -->
-            <div class="col-lg-8">
+        <!-- Blog Post Content Column -->
+        <div class="col-lg-8">
+            <?php
+            if (isset($_GET['p_id'])) {
+                $post_id = $_GET['p_id'];
+                $query = "SELECT * FROM posts WHERE post_id = {$post_id}";
+                $all_posts = mysqli_query($connection, $query);
+                while ($row = mysqli_fetch_assoc($all_posts)) {
+                    $post_id = $row['post_id'];
+                    $post_title = $row['post_title'];
+                    $post_content = $row['content'];
+                    $post_author = $row['author_id'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    ?>
+                    <h1><a href='post.php?p_id=<?php echo $post_id; ?>'><?php echo $post_title; ?></a></h1>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $post_author; ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content; ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <?php }
+        }
+        ?>
+            <hr>
 
-                <!-- Blog Post -->
-
-                <!-- Title -->
-                <h1>Blog Post Title</h1>
-
-                <!-- Author -->
-                <p class="lead">
-                    by <a href="#">Start Bootstrap</a>
-                </p>
-
-                <hr>
-
-                <!-- Date/Time -->
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 24, 2013 at 9:00 PM</p>
-
-                <hr>
-
-                <!-- Preview Image -->
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-
-                <hr>
-
-                <!-- Post Content -->
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
-
-                <hr>
-
-                <!-- Blog Comments -->
-
-                <!-- Comments Form -->
-                <div class="well">
-                    <h4>Leave a Comment:</h4>
-                    <form role="form">
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
-
-                <hr>
-
-                <!-- Posted Comments -->
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    </div>
-                </div>
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Nested Start Bootstrap
-                                    <small>August 25, 2014 at 9:30 PM</small>
-                                </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
-                        </div>
-                        <!-- End Nested Comment -->
-                    </div>
-                </div>
-
-            </div>
-
-            <!-- Blog Sidebar Widgets Column -->
-            <?php include "includes/sidebar.php"; ?>
+            <!-- Blog Comments -->
+            <!-- <?php include "comments.php"; ?> -->
 
         </div>
-        <!-- /.row -->
 
-        <hr>
+        <!-- Blog Sidebar Widgets Column -->
+        <?php include "includes/sidebar.php"; ?>
 
-        <!-- Footer -->
-        <?php include "includes/footer.php"; ?>
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Footer -->
+    <?php include "includes/footer.php"; ?>

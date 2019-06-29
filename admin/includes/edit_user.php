78bc4758bb96d389d84cbc59267ca88d4fa25f4e
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-06-29 23:51:55 +0200

    organizing spacing

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -9,7 +9,7 @@ $all_users = mysqli_query($connection, $query);
 if (!$all_users) {
     die("QUERY FAILED! " . mysqli_error($connection));
 }
-while($row = mysqli_fetch_assoc($all_users)){
+while ($row = mysqli_fetch_assoc($all_users)) {
     $id = $row['user_id'];
     $username = $row['user_name'];
     $first_name = $row['first_name'];
@@ -18,57 +18,57 @@ while($row = mysqli_fetch_assoc($all_users)){
     $image = $row['image'];
     $role = $row['role'];
     $password = $row['password'];
-?>
-<div class="container">
-    <form class="col-xs-6" action="" method="post" enctype="multipart/form-data">
-        <div class="form-group">
-            <label for="first_name">First Name</label>
-            <input type="text" class="form-control" name="first_name" value=<?php echo $first_name; ?>>
-        </div>
+    ?>
+    <div class="container">
+        <form class="col-xs-6" action="" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="first_name">First Name</label>
+                <input type="text" class="form-control" name="first_name" value=<?php echo $first_name; ?>>
+            </div>
 
-        <div class="form-group">
-            <label for="last_name">Last Name</label>
-            <input type="text" class="form-control" name="last_name" value=<?php echo $last_name; ?>>
-        </div>
+            <div class="form-group">
+                <label for="last_name">Last Name</label>
+                <input type="text" class="form-control" name="last_name" value=<?php echo $last_name; ?>>
+            </div>
 
-        <div class="form-group">
-            <label for="user_name">User Name</label>
-            <input type="text" class="form-control" name="user_name" value=<?php echo $username; ?>>
-        </div>
+            <div class="form-group">
+                <label for="user_name">User Name</label>
+                <input type="text" class="form-control" name="user_name" value=<?php echo $username; ?>>
+            </div>
 
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" name="password" value=<?php echo $password; ?>>
-        </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" name="password" value=<?php echo $password; ?>>
+            </div>
 
-        <div class="form-group">
-            <label for="post_image">User Image</label><br>
-            <img src="../images/<?php echo $image; ?>" alt="PHOTO" width="100">
-            <input type="file" class="form-control" name="image" >
-        </div>
+            <div class="form-group">
+                <label for="post_image">User Image</label><br>
+                <img src="../images/<?php echo $image; ?>" alt="PHOTO" width="100">
+                <input type="file" class="form-control" name="image">
+            </div>
 
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" name="email" value=<?php echo $email; ?>>
-        </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" name="email" value=<?php echo $email; ?>>
+            </div>
 
-        <div class="form-group">
-            <label for="role">Role</label>
-            <select name="role" id="role" class="form-control">
-                <option value="admin">Admin</option>
-                <option value="member">Member</option>
-            </select>
-        </div>
+            <div class="form-group">
+                <label for="role">Role</label>
+                <select name="role" id="role" class="form-control">
+                    <option value="admin">Admin</option>
+                    <option value="member">Member</option>
+                </select>
+            </div>
 
-        <div class="form-group">
-            <input type="submit" class="form-control btn btn-primary" name="submit" value="Update User">
-        </div>
+            <div class="form-group">
+                <input type="submit" class="form-control btn btn-primary" name="submit" value="Update User">
+            </div>
 
-    </form>
-</div>
-<?php 
-} 
-if(isset($_POST['submit'])){
+        </form>
+    </div>
+<?php
+}
+if (isset($_POST['submit'])) {
     updateUser();
 }
 ?>

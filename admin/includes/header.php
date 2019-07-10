481c824b3491e2288882e1cdf993518a7ff0710b
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-07-10 20:09:47 +0200

    adding CKEditor5 'WYSIWYG' to add post page

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -45,7 +45,8 @@ else{
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
+    <script src="https://cdn.ckeditor.com/ckeditor5/12.2.0/classic/ckeditor.js"></script>
+    <script src="js/scripts.js"></script>
 </head>
 
 <body>

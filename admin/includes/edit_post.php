cae6577c2ca13d13576791ed2fe4a5aa01f68ff1
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-07-10 22:20:16 +0200

    adding CKEditor5 'WYSIWYG' to edit post page

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -64,7 +64,7 @@ if (isset($_GET)) {
 
                 <div class="form-group">
                     <label for="post_content">Post Content</label>
-                    <textarea class="form-control" name="post_content" id="" cols="30" rows="10">
+                    <textarea class="form-control" name="post_content" id="body" cols="30" rows="10">
                             <?php echo $content; ?>
                             </textarea>
                 </div>
--- admin/includes/header.php
+++ admin/includes/header.php
@@ -46,7 +46,7 @@ else{
     <![endif]-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/12.2.0/classic/ckeditor.js"></script>
-    <script src="js/scripts.js"></script>
+    
 </head>
 
 <body>
